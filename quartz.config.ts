dceddc4cc4a2b38f3b443a98d168b9e4db05dd16
9inecarpediem <9inecarpediem@gamil.com>
2026-03-06 18:46:11 +0800

    Quartz sync: Mar 6, 2026, 6:46 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "2026北意自驾全攻略",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
